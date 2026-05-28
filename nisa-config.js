6bf1bfc6f7397c67a98a29e27db3a5ad6e0533dd
Alip961031 <kgurun88@gmail.com>
2026-05-28 11:25:12 +0800

    Delete nisa-config.js

--- nisa-config.js
+++
@@ -1,7 +0,0 @@
-// NISA CONFIG FILE
-// Edit URLs here. Do not put secret API keys in this file.
-window.NISA_CONFIG = {
-  DEEPSEEK_PROXY_URL: "https://nisa-deepseek-proxy.kgurun88.workers.dev",
-  KNOWLEDGE_URL: "./knowledge.json",
-  TAWK_EMBED_URL: "https://embed.tawk.to/YOUR_PROPERTY_ID/YOUR_WIDGET_ID"
-};
