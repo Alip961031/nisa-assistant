2f7c85fc0d905195af09ffb5964b2a4abc54dcee
Alip961031 <kgurun88@gmail.com>
2026-06-04 21:22:01 +0800

    Add files via upload

---
+++ nisa-config.js
@@ -0,0 +1,9 @@
+window.NISA_CONFIG = {
+  WORKER_URL: "https://nisa-deepseek-proxy.kgurun88.workers.dev",
+  DEEPSEEK_PROXY_URL: "https://nisa-deepseek-proxy.kgurun88.workers.dev",
+  KNOWLEDGE_URL: "./knowledge.json",
+  LIVE_CHAT_PROVIDER: "NISA_SUPPORT_HUB",
+  SUPPORT_HUB_ENABLED: true,
+  TAWK_REMOVED: true,
+  MAX_ATTACHMENT_PREVIEW_MB: 50
+};
